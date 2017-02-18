eab14fbaa6e118c4ea7991204a64ee3f19423cf0
mark-37 <safdaransari37@gmail.com>
2017-02-19 02:21:33 +0530

    First commit

---
+++ js/engine.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+$('input#search').autocomplete({ 
+   data: {
+      "Apple": 'Iam An Apple',
+      "Agnipath": null,
+      "Aanant": null,
+      "Ashu": null,
+       "Alibaba": null,
+      "Microsoft": null,
+      "Google": 'http://placehold.it/250x250'
+    }
+  });
+
+});
